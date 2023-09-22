28ed15bbc9018a897964af4815b565eedae7733e
ashish4323 <ashish.k@fwc.co.in>
2023-09-22 13:19:42 +0530

    Delted message

--- graph/index.js
+++ graph/index.js
@@ -1,15 +1,13 @@
 import { ApolloServer } from "@apollo/server";
-
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 // importing the ql utils
-import {Auth} from "./auth/index.js"
-import {Connection} from "./connection/index.js"
+import { Auth } from "./auth/index.js";
+import { Connection } from "./connection/index.js";
 
 // creating graphql apolo server and exporting so that can be linked withe express server
 async function createApolloGraphqlServer() {
-
-    const qlServer = new ApolloServer({
-        typeDefs:
-        `
+  const qlServer = new ApolloServer({
+    typeDefs: `
             ${Auth.typeDefs}
             ${Connection.typedef}
             
@@ -31,30 +29,29 @@ async function createApolloGraphqlServer() {
                ${Connection.mutations}
             }
         `,
-        resolvers:
-        {
-            User: {
-                ...Auth.resolvers.userResolvers
-            },
-            Connections : {
-                ...Connection.resolvers.connectionTypesResolvers
-            },
-            Query : {
-                ...Auth.resolvers.queries,
-                ...Connection.resolvers.queries
-      
-            },
-            Mutation: {
-               ...Auth.resolvers.mutations,
-               ...Connection.resolvers.mutations
-            }
-        }
-    },)
+    resolvers: {
+      User: {
+        ...Auth.resolvers.userResolvers,
+      },
+      Connections: {
+        ...Connection.resolvers.connectionTypesResolvers,
+      },
+      Query: {
+        ...Auth.resolvers.queries,
+        ...Connection.resolvers.queries,
+      },
+      Mutation: {
+        ...Auth.resolvers.mutations,
+        ...Connection.resolvers.mutations,
+      },
+    },
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+  });
 
-    await qlServer.start();
+  await qlServer.start();
 
-
-    return qlServer
+  return qlServer;
 }
 
-export default createApolloGraphqlServer
+export default createApolloGraphqlServer;
--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-import express from "express"
-import cors from "cors"
-import createApolloGraphqlServer from "./graph/index.js"
+import express from "express";
+import cors from "cors";
+import createApolloGraphqlServer from "./graph/index.js";
 import { expressMiddleware } from "@apollo/server/express4";
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.mjs"
-import {decodeTokenAndFetchUser} from "./contexts/auth.js"
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.mjs";
+import { decodeTokenAndFetchUser } from "./contexts/auth.js";
 
+async function init() {
+  const app = express();
+  const PORT = process.env.PORT || 8000;
 
+  dotenv.config();
+  app.use(express.json());
+  app.use(cors());
 
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running" });
+  });
 
-async function init()
-{
-    const app = express();
-    const PORT = process.env.PORT || 8000
-
-    dotenv.config()
-    app.use(express.json())
-    app.use(cors())
-
-
-    app.get("/", (req, res) => {
-        res.json({ message: "Server is up and running" });
-    });
-
-    // connection to database
-    mongoose.connect(process.env.CONNECTION_URL)
+  // connection to database
+  mongoose
+    .connect(process.env.CONNECTION_URL)
     .then(() => console.log("Database Connected"))
-    .catch(() => console.log("Some error occured "))
+    .catch(() => console.log("Some error occured "));
 
-    // setting up the graphql server
-    app.use(graphqlUploadExpress())
-    app.use("/graphql",expressMiddleware(await createApolloGraphqlServer(), 
-                    {
-                        context: decodeTokenAndFetchUser
-                    }
-            ))
-    
+  // setting up the graphql server
+  app.use(graphqlUploadExpress());
+  app.use(
+    "/graphql",
+    expressMiddleware(await createApolloGraphqlServer(), {
+      context: decodeTokenAndFetchUser,
+    })
+  );
 
-    // starting the server
-    app.listen(PORT , () => `Server is Running at PORT : ${8000}`)
-
+  // starting the server
+  app.listen(PORT, () => `Server is Running at PORT : ${8000}`);
 }
 
-init()
+init();
